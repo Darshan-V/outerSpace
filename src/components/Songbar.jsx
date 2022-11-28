881528a3cea571d620f0e7d8afebd375c2941cf8
Darshan-V <mr.darshanv@gmail.com>
2022-11-28 12:34:56 +0530

    created songbar

--- src/components/Songbar.jsx
+++ src/components/Songbar.jsx
@@ -10,7 +10,44 @@ const Songbar = ({
   handlePauseClick,
   handlePlayClick,
 }) => {
-  return <div></div>
+  return (
+    <div className="SongbarforActiveSong">
+      <div className="songImage">
+        <img
+          className="songImg"
+          src={
+            artistId
+              ? song.attributes.artwork.url
+                  .replace('{w}', '125')
+                  .replace('{h}', '125')
+              : song.images.coverart
+          }
+          alt={song.title}
+        />
+        <div className="Artist">
+          {!artistId ? (
+            <Link to={`songs/${song.key}`}>
+              <p className="songTitle">{song.title}</p>
+            </Link>
+          ) : (
+            <p className="SongAltTitle">{song.attributes.name}</p>
+          )}
+          <p className="songMeta">
+            {artistId ? song.attributes.albumName : song.subtitle}
+          </p>
+        </div>
+      </div>
+      {!artistId ? (
+        <PlayPause
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          song={song}
+          handlePause={handlePauseClick}
+          handlePlay={() => handlePlayClick(song, i)}
+        />
+      ) : null}
+    </div>
+  )
 }
 
 export default Songbar
